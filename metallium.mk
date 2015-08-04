98011263c220b3db11ce0b6512607a6453c81a4a
Rados B. <Buda1996@users.noreply.github.com>
2015-08-04 11:41:09 +0200

    Update and rename fusionsp.mk to metallium.mk

--- metallium.mk
+++ metallium.mk
@@ -1,10 +1,10 @@
 $(call inherit-product, device/samsung/ks01lte/full_ks01lte.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/fusion/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/metalllium/config/nfc_enhanced.mk)
 
 # Inherit some common Fusion stuff.
-$(call inherit-product, vendor/fusion/config/common_phone.mk)
+$(call inherit-product, vendor/metallium/config/common_phone.mk)
 
 PRODUCT_DEVICE := ks01lte
 PRODUCT_NAME := ks01lte
